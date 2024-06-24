f80d4e4f5a9ccc36fc543dad61a8e4ec9a498065
nikhita-shetty <nikkipshetty123@gmail.com>
2024-06-25 00:04:36 +0530

    create a sample website

---
+++ menu.js
@@ -0,0 +1,13 @@
+let b1 = document.querySelector("#b1");
+
+b1.addEventListener(onclick,()=>{
+    let res = document.querySelector("#b1").innerText;
+    console.log(res);
+
+})
+
+
+// function myfunction(){
+//     let r = document.querySelector("button").innerText;
+//     console.log(r);
+// }
